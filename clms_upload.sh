2734165147e743bb97e3f3d5ac96757594e47aa0
Jan Musial <jmusial@cloudferro.com>
2025-11-12 17:18:07 +0100

    Update clms_upload.sh
    change .tar support logic, add product priority based on temporalRepeatRate, EEA versioning support, fallback to WAW3-2 region if WAW3-1 is unavailable

--- clms_upload.sh
+++ clms_upload.sh
@@ -8,8 +8,9 @@
 # Version 1.04 [20250723] rename "last_modified" attribute which denotes modification time in CLMS producer local storage to "created"
 # Version 1.05 [20250827] proper handling of whitespaces in local path
 # Version 1.06 [20250901] update of the UTM zone of uploaded products, reformating of rclone command, addition of the --retries-sleep --tps-limit to rclone 
+# Version 1.07 [20251112] change .tar support logic, add product priority based on temporalRepeatRate, EEA versioning support, fallback to WAW3-2 region if WAW3-1 is unavailable 
 ###############################
-version="1.06"
+version="1.07"
 usage()
 {
 cat << EOF
@@ -27,14 +28,8 @@ export RCLONE_CONFIG_CLMS_PROVIDER='Ceph'
 # Single file upload:
 clms_upload.sh -b CLMS-YOUR-BUCKET-NAME -l "/tmp/c_gls_NDVI_200503110000_GLOBE_VGT_V3.0.1.nc"
 #
-# Upload directory of a CLMS product containing multiple files:
-clms_upload.sh -b CLMS-YOUR-BUCKET-NAME -l "/tmp/c_gls_NDVI_202001010000_GLOBE_PROBAV_V3.0.2.cog/" -t
-#
 #Batch upload of all NetCDF files residing localy in /home/ubuntu directory in 5 parallel sessions:
 find /home/ubuntu -name "*.nc" | xargs -l -P 5 bash -c 'clms_upload.sh -b CLMS-YOUR-BUCKET-NAME -l "$0"'
-#
-#Batch upload of all COG folders residing localy in /home/ubuntu directory in 5 parallel sessions:
-find /home/ubuntu -name "*.cog" -type d | xargs -l -P 5 bash -c 'clms_upload.sh -b CLMS-YOUR-BUCKET-NAME -l "$0" -t'
 #
 ################################################################
 OPTIONS:
@@ -42,17 +37,13 @@ OPTIONS:
    -h      this message
    -l      [REQUIRED] local path (i.e. file system) path to input file or a directory with CLMS product name containing product files (e.g. COGs & STAC JSON metadata) 
    -o      [OPTIONAL] shall input file in the CLMS-YOUR-BUCKET-NAME bucket in the CDSE staging storage be overwritten?
-   -p      [OPTIONAL] job priority ranging 0-9. Higher priority indicates that a CLMS product will be ingested faster. Default 0.  
+   -p      [OPTIONAL] job priority ranging 0-9. Higher priority indicates that a CLMS product will be ingested faster. Default 3.  
    -r      [OPTIONAL] product name(s) to be replaced/patched by the product to uploaded. 
 		   If more than one product needs to be replaced (e.g. NetCDF & COGs) than comma-separated list of names should be provided.
-   -t      [CONDITIONALLY REQUIRED] if a CLMS product is a directory containing many files. This flag indicates that a CLMS folder should be combined as a TAR archive.  
    -v      clms_upload.sh version
-
 EOF
 }
-priority=0
-taring=0
-while getopts “b:l:p:r:hotv” OPTION; do
+while getopts “b:l:p:r:hov” OPTION; do
 	case $OPTION in
 		b)
 			bucket=$OPTARG
@@ -73,9 +64,6 @@ while getopts “b:l:p:r:hotv” OPTION; do
 		r)  
 			rep=$OPTARG
 			;;
-		t)  
-			taring=1
-			;;
 		v)
 			echo version $version
 			exit 0
@@ -90,52 +78,51 @@ done
 #verify if bucket was set
 if [ -z $bucket ]; then
 	echo "ERROR: Bucket name must be specified!"
-	exit 1
+	exit 2
 fi
 #verify if local_file was set
 if [ -z "$local_file" ]; then
 	echo "ERROR: Local path must be specified!"
-	exit 2
+	exit 3
 fi
 #verify if environmental variables were set
 if [ -z $RCLONE_CONFIG_CLMS_TYPE ] || [ -z $RCLONE_CONFIG_CLMS_ACCESS_KEY_ID ] || [ -z $RCLONE_CONFIG_CLMS_SECRET_ACCESS_KEY ] || [ -z $RCLONE_CONFIG_CLMS_REGION ] || [ -z $RCLONE_CONFIG_CLMS_ENDPOINT ] || [ -z $RCLONE_CONFIG_CLMS_PROVIDER ]; then
 	echo "ERROR: Some environmental variables starting with RCLONE_CONFIG_CLMS_XXXXX were not set!"
-	exit 3
+	exit 4
 fi
-#verify if file/folder exists in the local storage
-if [ $taring == 1 ]; then
-	if [ ! -d  "$local_file" ] ; then
-		echo "ERROR: Directory $local_file does not exist in the local storage!"
-		exit 4
-	fi
-else
-	if [ ! -f "$local_file" ] ; then
-		echo "ERROR: File $local_file does not exist in the local storage!"
-		exit 5
-	fi
+#verify if file exists in the local storage
+if [ ! -f "$local_file" ] ; then
+    echo "ERROR: File $local_file does not exist in the local storage!"
+    exit 5
 fi
 
 #verify if the product exists already in the CDSE OData 
-odata_product_count=$(wget -qO - 'https://datahub.creodias.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "$local_file" | rev | cut -f 2- -d '.' | rev)'%27))' | jq '.value | length')
+odata_product_count=$(wget -qO - 'https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "${local_file%.*}")'%27))' | jq '.value | length')
 if [ $odata_product_count -gt 0 ]; then
 	echo 'ERROR: Such product exists in the CDSE!'
 	exit 6
 fi
 #verify if the product has not been already deleted from the CDSE
-deleted_product_count=$(wget -qO - 'https://datahub.creodias.eu/odata/v1/DeletedProducts?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "$local_file" | rev | cut -f 2- -d '.' | rev)'%27))' | jq '.value | length')
+deleted_product_count=$(wget -qO - 'https://catalogue.dataspace.copernicus.eu/odata/v1/DeletedProducts?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "${local_file%.*}")'%27))' | jq '.value | length')
 if [ $deleted_product_count -gt 0 ]; then
 	echo 'ERROR: Such product has been deleted from the CDSE!'
 	exit 7
 fi
 #verify if the product to be uploaded is readable by gdal
-if [ $taring == 1 ]; then
-	for gdal_product in $(find "$local_file" -name '*.tif' -o -name '*.tiff' -o -name '*.nc'); do
-		gdalinfo $gdal_product > /dev/null 2>&1
+if [ "${local_file##*.}" == "tar" ]; then
+    #add test if tar has a correctly named internal directory
+    if [ $(tar tf $local_file | grep -c "./$(basename ${local_file%.*})/") -eq 0 ]; then
+        echo "ERROR: TAR file does not contain the folder named '$(basename ${local_file%.*})' containing all the product files."
+        exit 8
+    fi
+	for gdal_product in $(tar tf $local_file | grep -E '.tif|.nc' | grep -vE '.tif.|.nc.' |  cut -c3- | sed "s|^|${local_file}/|"); do
+		echo $gdal_product
+		gdalinfo /vsitar/${gdal_product} > /dev/null 2>&1
 		if [ $? -ne 0 ]; then
-			ogrinfo $gdal_product > /dev/null 2>&1
+			ogrinfo /vsitar/${gdal_product} > /dev/null 2>&1
 			if [ $? -ne 0 ]; then
 				echo "ERROR: GDAL can not open ${gdal_product}!"
-				exit 8
+				exit 9
 			fi
 		fi
 	done
@@ -145,61 +132,101 @@ else
 		ogrinfo "$local_file" > /dev/null 2>&1
 		if [ $? -ne 0 ]; then
 			echo "ERROR: GDAL can not open ${local_file}!"
-			exit 8
+			exit 10
 		fi
 	fi
 fi
 
-if [ $(basename "$local_file" | tr -dc '.' | wc -c) -eq 3 ] || ([ $taring == 1 ] && [ $(basename "$local_file" | tr -dc '.' | wc -c) -eq 2 ]); then
+
+#verify if json STAC file within a .tar product is valid
+if [ "${local_file##*.}" == "tar" ]; then
+	for stac_json in $(tar tf $local_file | grep '_stac.json'); do
+		tar -xOf $local_file $stac_json | jq . >/dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			echo "ERROR: STAC JSON is invalid. Can not parse ${stac_json}!"
+            exit 11
+		fi
+	done
+fi
+#check JRC product versioning
+if [ $(basename "$local_file" | tr -dc '.' | wc -c) -eq 3 ]; then
 	#verify path number is not 0
-	patch_number=$(echo "$local_file" | rev | cut -f 2 -d '.')
+	patch_number=$(echo "$local_file" | rev | cut -f 2 -d '.' | tr -cd '0-9')
 	if [ $patch_number -eq 0 ]; then
 		echo "ERROR: Patch number (i.e. last digit in version) has to start with 1"
-		exit 9
+		exit 12
 	fi
-	product_to_replace=''
 	#verify if the previous product version exists
-	odata_product=$(wget -qO - 'https://datahub.creodias.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "$local_file" | rev | cut -f 3- -d '.' | rev)'%27))')
+	odata_product=$(wget -qO - 'https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "$local_file" | rev | cut -f 3- -d '.' | rev)'%27))&$orderby=Name%20desc')
 	if [ $(printf "$odata_product" | jq '.value|length') -gt 0 ]; then
-		product_to_replace=$(printf "$odata_product" |  jq -r '.value[].Name' | paste -sd, -)
+		if [ -z $rep ]; then
+			product_to_replace=$(printf "$odata_product" |  jq -r '.value[].Name' | paste -sd, -)
+		fi
 		odata_patch_number=$(printf "$odata_product" |  jq '.value[0].Name' | rev | cut -f 1 -d '.' | tr -dc '0-9')
 		if [ $patch_number -ne $((${odata_patch_number}+1)) ]; then
 			echo "ERROR: Patch version in CDSE is ${odata_patch_number} and the patch number of the product uploaded product should be $((${odata_patch_number}+1)) but it is ${patch_number}!"
-			exit 11
+			exit 13
 		fi
 	fi
 fi
+#check EEA product versioning
+if [[ "$local_file" =~ ^.*V[0-9]{3}$ ]]; then
+	patch_number=$(echo ${local_file%.*} | tail -c 3 | bc)
+	#verify if the previous product version exists
+	odata_product=$(wget -qO - 'https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "${local_file%.*}" | sed "s/..$//")'%27))&$orderby=Name%20desc')
+	if [ $(printf "$odata_product" | jq '.value|length') -gt 0 ]; then
+		if [ -z $rep ]; then
+			product_to_replace=$(printf "$odata_product" |  jq -r '.value[].Name' | paste -sd, -)
+		fi
+		odata_patch_number=$(printf "$odata_product" |  jq '.value[0].Name' | tail -c 3 | bc)
+		if [ $patch_number -ne $((${odata_patch_number}+1)) ]; then
+			echo "ERROR: Patch version in CDSE is ${odata_patch_number} and the patch number of the product uploaded product should be $((${odata_patch_number}+1)) but it is ${patch_number}!"
+			exit 14
+		fi
+	fi
+fi
+
 #verify product to replace
 if [ ! -z $rep ]; then
-	rep_product=$(wget -qO - 'https://datahub.creodias.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename $rep | rev | cut -f 2- -d '.' | rev)'%27))')
+	rep_product=$(wget -qO - 'https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename $rep | rev | cut -f 2- -d '.' | rev)'%27))')
 	if [ $(printf "$rep_product" | jq '.value|length') -gt 0 ]; then
 		product_to_replace=$(printf "$rep_product" |  jq -r '.value[].Name' | paste -sd, -)
 	else
-		echo "ERROR: Product to patched does not exist in the CDSE: $rep"
-		exit 12
+		echo "ERROR: Product to be patched does not exist in the CDSE: $rep"
+		exit 15
 	fi
 fi
+
 #print products to be replaced
 if [ ! -z $product_to_replace ]; then
 	echo "INFO: Following products will be patched in the CDSE: $product_to_replace"
 fi
 
-last_modified=$(date -u -r "$local_file" '+%Y-%m-%dT%H:%M:%SZ')
-#tar directory if needed
-if [ $taring == 1 ]; then
-	echo "INFO: Taring directory: $local_file"
-	cd "${local_file}"
-	tar cf "/tmp/"$(basename "${local_file}" | sed 's/\(.*\)\./\1_/')".tar -C $local_file" .
-	if [ $? -ne 0 ];then
-		echo "ERROR: Could not tar the directory: $local_file"
-		exit 13
-	fi
-	local_file="/tmp/$(basename ${local_file} | sed 's/\(.*\)\./\1_/').tar"
+#try to set ingestion priority for the product to be uploaded
+if [ -z "$priority" ]; then
+    priority=3
+    odata_product=$(wget -qO - 'https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=(Collection/Name%20eq%20%27CLMS%27%20and%20startswith(Name,%27'$(basename "${local_file}" | cut -f 1-3 -d "_")'%27))&$top=1&$expand=Attributes')
+    temporalRepeatRate=$(echo "$odata_product" | jq -r '.value[].Attributes[] | select(.Name=="temporalRepeatRate") | .Value')
+    case "$temporalRepeatRate" in
+        hourly)
+            priority=7
+            ;;
+        daily)
+            priority=5
+            ;;
+    esac
 fi
+#check if https://s3.waw3-1.cloudferro.com if not fall back to https://s3.waw3-2.cloudferro.com
+wget -q --spider "$RCLONE_CONFIG_CLMS_ENDPOINT" || RCLONE_CONFIG_CLMS_ENDPOINT='https://s3.waw3-2.cloudferro.com'
+
+#extract technical attributes for S3
+last_modified=$(date -u -r "$local_file" '+%Y-%m-%dT%H:%M:%SZ')
 s3_path=${bucket}$(date -u --date now '+/%Y/%m/%d')
 timestamp=$(date -u -d now '+%Y-%m-%dT%H:%M:%SZ')
 file_size=$(du -smb --apparent-size "$local_file" | cut -f1)
 md5_checksum=$(md5sum -b "$local_file" | cut -c-32)
+
+#upload product
 rclone -q copy \
 --s3-disable-http2 \
 --s3-no-check-bucket \
@@ -223,13 +250,10 @@ rclone -q copy \
 --metadata-set source-cleanup=true \
 --metadata-set product-to-replace=${product_to_replace}${overwrite} "$local_file" CLMS:$s3_path
 rclone_exit_code=$?
-if [ $taring == 1 ]; then
-	rm -v "/tmp/$(basename ${local_file})"
-fi
 if [ $rclone_exit_code != 0 ]; then
 	echo "ERROR: rclone exit code:$rclone_exit_code. Failed to upload $local_file to s3://${s3_path}"
- 	exit 14
+ 	exit 16
 else 
-	echo "SUCCESS: Uploaded $local_file to s3://${s3_path}"
+	echo "SUCCESS: Uploaded $local_file to s3://${s3_path} in ${RCLONE_CONFIG_CLMS_ENDPOINT} endpoint!"
  	exit 0
 fi
